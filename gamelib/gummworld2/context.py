9782be68d153eea88d47a327f0791326db6ba2bf
stabbingfinger@gmail.com <stabbingfinger@gmail.com@b84ff5bd-9abf-0679-1f5e-323cf1bf72f5>
2011-04-27 14:34:57 +0000

    Changed: docstring; call superclass.__init__.

--- gamelib/gummworld2/context.py
+++ gamelib/gummworld2/context.py
@@ -1,6 +1,32 @@
+#!/usr/bin/env python
+
+# This file is part of Gummworld2.
+#
+# Gummworld2 is free software: you can redistribute it and/or modify it
+# under the terms of the GNU Lesser General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Gummworld2 is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with Gummworld2.  If not, see <http://www.gnu.org/licenses/>.
+
+
+__version__ = '$Id$'
+__author__ = 'Gummbum, (c) 2011'
+
+
+__doc__ = """context.py - A simple context stack for Gummworld2.
+"""
+
+
 class Context(object):
     def __init__(self):
-        pass
+        object.__init__(self)
     def update(self, dt):
         """Called once per frame"""
         pass
