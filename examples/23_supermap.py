b73d2deb058e6c0b82313e1bc1f77894e1fa0c99
stabbingfinger@gmail.com <stabbingfinger@gmail.com@b84ff5bd-9abf-0679-1f5e-323cf1bf72f5>
2011-06-06 14:33:55 +0000

    Fixed caching and edge-collapse problems in helperspygame.py. Polished 23_supermap.py a bit.

--- examples/23_supermap.py
+++ examples/23_supermap.py
@@ -9,6 +9,9 @@ from gummworld2 import Engine, State, View, SuperMap, MapHandler, TiledMap, Stat
 from gummworld2 import context, data, toolkit, model, supermap
 
 
+COLLAPSE_KEYS = range(K_1, K_9+1)
+
+
 class App(Engine):
     
     def __init__(self):
@@ -33,12 +36,14 @@ class App(Engine):
             'Visible %s', callback=lambda:str(len(State.map.visible_maps)), interval=.5))
         State.hud.add('History', Statf(State.hud.next_pos(),
             'History %s', callback=lambda:'%s/%s'%(str(len(State.map.history)),str(State.map.max_maps)), interval=.5))
+        State.hud.add('Collapse Level', Statf(State.hud.next_pos(),
+            'Collapse Level %s', callback=lambda:'%d'%State.map.current.collapse_level, interval=.5))
         State.clock.schedule_update_priority(State.hud.update, 1.0)
     
     def update(self, dt):
         if self.movex or self.movey:
             r = State.camera.rect.move(self.movex,self.movey)
-            #r.clamp_ip(State.map.rect)
+            r.clamp_ip(State.map.rect)
             State.camera.position = r.center
         State.map.update(dt)
     
@@ -53,6 +58,10 @@ class App(Engine):
         elif key == K_UP: self.movey += -State.speed
         elif key == K_RIGHT: self.movex += State.speed
         elif key == K_LEFT: self.movex += -State.speed
+        elif key in (COLLAPSE_KEYS):
+            clevel = key - K_0
+            for mh in State.map.handlers.values():
+                mh.collapse(clevel)
         elif key == K_ESCAPE: context.pop()
     
     def on_key_up(self, key, mod):
@@ -67,12 +76,23 @@ class App(Engine):
 
 class TiledMapHandler(MapHandler):
     
+    # 7 gives the best FPS at resolution 320x320, but loading causes hiccup
+    # 2 gives smoothest loading, highest cache hits
+    collapse_level = 2
+    
     def load(self):
         self.map = TiledMap(data.filepath('map',self.map_file))
-        #self.map.get_layer(0).collapse(8)
+        self.collapse(self.collapse_level)
+    
+    def collapse(self, clevel):
+        self.collapse_level = clevel
+        if self.map:
+            for layer in self.map.get_tile_layers():
+                layer.collapse(clevel)
 
 
 def make_supermap(app):
+    # Make a 9x9 supermap, using the same map file for each.
     app.map = State.map = SuperMap()
     for n in ((0,0),) + supermap.NEIGHBORS:
         app.map.add_handler(TiledMapHandler(n, 'Gumm multi layer.tmx'))
